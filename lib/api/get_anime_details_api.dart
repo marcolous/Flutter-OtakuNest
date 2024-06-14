dcfe9763fa2b84638f800ec029ac3f53b894a2ca
marco <119810706+array-marco@users.noreply.github.com>
2024-06-14 20:27:05 +0300

    create get anime details api and get seasonal animes api

---
+++ lib/api/get_anime_details_api.dart
@@ -0,0 +1,27 @@
+import 'dart:convert';
+
+import 'package:anime_world_tutorial/config/app_config.dart';
+import 'package:anime_world_tutorial/constants/constants.dart';
+import 'package:anime_world_tutorial/models/anime_details.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+Future<AnimeDetails> getAnimeDetailsApi({required String id}) async {
+  final detailsUrl =
+      '${Constants.baseUrl}/$id?fields=id,title,main_picture,alternative_titles,start_date,end_date,synopsis,mean,rank,popularity,num_list_users,num_scoring_users,nsfw,created_at,updated_at,media_type,status,genres,my_list_status,num_episodes,start_season,broadcast,source,average_episode_duration,rating,pictures,background,related_anime,related_manga,recommendations,studios,statistics';
+
+  final response = await http.get(
+    Uri.parse(detailsUrl),
+    headers: {'X-MAL-CLIENT-ID': clientId},
+  );
+
+  if (response.statusCode == 200) {
+    final Map<String, dynamic> data = jsonDecode(response.body);
+    final animeDetails = AnimeDetails.fromJson(data);
+    return animeDetails;
+  } else {
+    debugPrint('Error : ${response.statusCode}');
+    debugPrint('Body : ${response.body}');
+    throw Exception('Failed to get data!');
+  }
+}
---
+++ lib/api/get_seasonal_animes_api.dart
@@ -0,0 +1,30 @@
+import 'dart:convert';
+
+import 'package:anime_world_tutorial/common/utils/utils.dart';
+import 'package:anime_world_tutorial/config/app_config.dart';
+import 'package:anime_world_tutorial/models/anime.dart';
+import 'package:anime_world_tutorial/constants/constants.dart';
+import 'package:anime_world_tutorial/models/anime_info.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+Future<Iterable<Anime>> getSeasonalAnime({required int limit}) async {
+  final year = DateTime.now().year;
+  final season = getCurrentSeason();
+  final seasonUrl = '${Constants.baseUrl}/season/$year/$season?limit=$limit';
+
+  final response = await http.get(
+    Uri.parse(seasonUrl),
+    headers: {'X-MAL-CLIENT-ID': clientId},
+  );
+
+  if (response.statusCode == 200) {
+    final Map<String, dynamic> data = jsonDecode(response.body);
+    final seasonalAnime = AnimeInfo.fromJson(data);
+    return seasonalAnime.animes;
+  } else {
+    debugPrint('Error : ${response.statusCode}');
+    debugPrint('Body : ${response.body}');
+    throw Exception('Failed to get data!');
+  }
+}
